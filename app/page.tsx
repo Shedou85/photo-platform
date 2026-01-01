c96a79ab437a46a6708a16f3e2eba57747b9d1ac
Mrius Dainys <marius.dainys@pinetnet.io>
2026-01-01 11:06:55 +0200

    test for update

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,18 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-gray-100 font-sans text-gray-900 dark:bg-gray-900 dark:text-gray-100">
       <main className="flex flex-col items-center justify-center p-8 text-center max-w-2xl">
         {/* Removed: Image component */}
-        <h2 className="text-4xl font-extrabold text-green-600 mb-8">Viskas Veikia</h2> {/* Added PixelForge text */}
+        <h2 className="text-4xl font-extrabold text-green-600 mb-8">
+          Viskas Veikia 222
+        </h2>{" "}
+        {/* Added PixelForge text */}
         <h1 className="text-5xl font-bold mb-4">Projektas kuriamas!</h1>
         <p className="text-xl mb-6">
-          Šiuo metu aktyviai dirbame prie nuotraukų platformos, kuri leis lengvai dalintis, saugoti ir tvarkyti savo nuotraukas.
-          Mūsų tikslas – sukurti intuityvią ir galingą platformą fotografams ir nuotraukų mėgėjams.
+          Šiuo metu aktyviai dirbame prie nuotraukų platformos, kuri leis
+          lengvai dalintis, saugoti ir tvarkyti savo nuotraukas. Mūsų tikslas –
+          sukurti intuityvią ir galingą platformą fotografams ir nuotraukų
+          mėgėjams.
         </p>
-        <p className="text-lg">
-          Užsukite vėliau, kai projektas bus paruoštas!
-        </p>
+        <p className="text-lg">Užsukite vėliau, kai projektas bus paruoštas!</p>
         <p className="mt-8 text-sm text-gray-500 dark:text-gray-400">
           Ačiū už jūsų kantrybę!
         </p>
